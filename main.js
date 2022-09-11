8ae7b102b3475eabafbf850ede156be416b3c07e
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2022-09-11 21:53:33 +0530

    Javascript minion translation added

--- main.js
+++ main.js
@@ -2,9 +2,25 @@ var btntranslate = document.querySelector(".btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var txtoutput = document.querySelector(".txt-output");
 
+var url = "	https://api.funtranslations.com/translate/minion.json";
+
+function urlConstructor(input){
+    return url + "?" + "text=" + input;
+}
+
+function errorHandler(){
+    alert("Something Wrong with the Server. Try again later.")
+}
+
 function eventListenerHandler(){
-    console.log("click! your input was: " + txtinput.value);
-    txtoutput.innerText = "click! your input was: " + txtinput.value;
+    var inputtxt = txtinput.value;
+    fetch(urlConstructor(inputtxt))
+    .then(response => response.json())
+    .then(data => {
+        var txttranslation = data.contents.translated
+        txtoutput.innerText = txttranslation;
+        })
+    .catch(errorHandler);
 }
 
 btntranslate.addEventListener('click', eventListenerHandler);
